d552532c8a5949c8063929e56db38ea1b73249ac
mahirmaruf <mahir.maruf@gmail.com>
2019-03-08 16:46:23 -0500

    ex6

--- machine-learning-ex6/machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,8 +23,49 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Creating multiplicative steps for C and sigma
+C_step = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_step = [0.01 0.03 0.1 0.3 1 3 10 30];
+
+preds_error = zeros( length(C_step), length(sigma_step) );
+
+output_c_sigma = zeros( length(C_step) + length(sigma_step), 3 );
+
+row = 1;
 
 
+for c_i = 1:length(C_step)
+    for s_i = 1:length(sigma_step)
+        cTest = C_step(c_i);
+        sigmaTest = sigma_step(s_i);
+        
+        % Model training
+        model = svmTrain(X, y, cTest, @(x1, x2) gaussianKernel(x1, x2, sigmaTest));
+        
+        % predictions from SVM trained model
+        preds = svmPredict(model, Xval);
+        
+        % returns the prdiction error
+        preds_error(c_i,s_i) = mean( double( preds ~= yval));
+        
+        % Error for each C and sigma combination
+        output_c_sigma(1,:) = [preds_error(c_i,s_i), cTest, sigmaTest];
+        
+        row = row + 1; 
+    end
+end
+
+lowest_error = sortrows(output_c_sigma, 1);
+
+C = lowest_error(1,2);
+sigma = lowest_error(1,3);
+
+
+        
+        
+        
+        
+        
 
 
 
